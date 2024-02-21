884592bc115854de27b3e70b8862a73a9b2eaaab
dev-status-200 <dev.status.200@gmail.com>
2024-02-21 12:46:21 +0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps:{ session, ...pageProps }, }) {
 
   return (
     <>
-      {router.pathname !='/login' &&
+      {router.pathname!='/login' &&
         <Provider store={store}>
           <QueryClientProvider client={queryClient}>
             <MainLayout>
